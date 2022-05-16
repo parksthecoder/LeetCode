067e7e92d40ee2663b0a133ee75754ebcf0af71a
Brandon Parks <brandonparks2020@gmail.com>
2022-05-16 14:33:03 -0500

    Initial commit

---
+++ 13-romanToInteger.js
@@ -0,0 +1,52 @@
+//* result will be a sum of Roman Numerals as a number (0 - 3999)
+//* Roman To Integer
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  // const array = s.split()
+
+  const numerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let numeralSum = 0;
+
+  if (1 <= s.length <= 15) {
+    for (i = 0; i < s.length; i++) {
+      if (s[i] == "I" && s[i + 1] == "V") {
+        numeralSum += 4;
+        i++;
+      } else if (s[i] == "I" && s[i + 1] == "X") {
+        numeralSum += 9;
+        i++;
+      } else if (s[i] == "X" && s[i + 1] == "L") {
+        numeralSum += 40;
+        i++;
+      } else if (s[i] == "X" && s[i + 1] == "C") {
+        numeralSum += 90;
+        i++;
+      } else if (s[i] == "C" && s[i + 1] == "D") {
+        numeralSum += 400;
+        i++;
+      } else if (s[i] == "C" && s[i + 1] == "M") {
+        numeralSum += 900;
+        i++;
+      } else {
+        numeralSum += numerals[s[i]];
+      }
+    }
+    return numeralSum;
+  }
+};
+
+console.log(romanToInt("XLIX"));
+console.log(romanToInt("LI"));
---
+++ practice.js
@@ -0,0 +1,9 @@
+// alert("I am JavaScript");
+
+
+const fruits = ["banana", "apple", "strawberry", "cherry", "watermelon"]
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i] + " Ice Cream");
+}
+
